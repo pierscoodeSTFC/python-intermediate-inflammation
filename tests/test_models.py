1f7a041bb8966e62f34dea639de18f3f3f53f3df
Piers <piers.coode@stfc.ac.uk>
2022-11-02 10:20:16 +0000

    Added some new tests for daily_min and daily_max

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,6 +3,8 @@
 import numpy as np
 import numpy.testing as npt
 
+from inflammation.models import daily_min
+
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
@@ -29,3 +31,54 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+
+def test_daily_max_zeros():
+    """Test that the max value works for an array of zeros."""
+    from inflammation.models import daily_max
+
+    test_input = np.array([[0, 0],
+                           [0, 0],
+                           [0, 0]])
+    test_result = np.array([0, 0])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+
+def test_daily_max_integers():
+    """Test that the max value works for an array of integers."""
+    from inflammation.models import daily_max
+
+    test_input = np.array([[1, 6],
+                           [3, 4],
+                           [5, 2]])
+    test_result = np.array([5, 6])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+
+def test_daily_min_zeros():
+    """Test that the max value works for an array of zeros."""
+    from inflammation.models import daily_min
+
+    test_input = np.array([[0, 0],
+                           [0, 0],
+                           [0, 0]])
+    test_result = np.array([0, 0])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_min(test_input), test_result)
+
+
+def test_daily_min_integers():
+    """Test that the max value works for an array of integers."""
+    from inflammation.models import daily_min
+
+    test_input = np.array([[1, 4],
+                           [3, 2],
+                           [5, 6]])
+    test_result = np.array([1, 2])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_min(test_input), test_result)
